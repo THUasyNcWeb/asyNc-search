08729f3e52f64bddb77df1a699fe8b85be77ee7b
abmfy <2021010787@secoder.net>
2022-10-12 01:17:22 +0800

    :rocket: chore: initial deployment

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM docker.elastic.co/elasticsearch/elasticsearch:7.10.2
+
+COPY . /res
+
+ENV discovery.type=single-node
+
+RUN sh /res/build.sh
+
+EXPOSE 9200 9300
---
+++ build.sh
@@ -0,0 +1,9 @@
+cd /usr/share/elasticsearch
+
+mv /res/elasticsearch.yml config/elasticsearch.yml
+
+cd plugins
+
+mkdir ik
+
+unzip /res/elasticsearch-analysis-ik-7.zip -d ik
